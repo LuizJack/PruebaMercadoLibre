d5618327842c59a79c15959fc0958e5c4224e454
LuizJack <104174477+LuizJack@users.noreply.github.com>
2022-12-05 14:04:58 -0500

    create project
    create project

---
+++ PruebaMercadiLibre/PruebaMercadiLibre/PruebaMercadiLibreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaMercadiLibreApp.swift
+//  PruebaMercadiLibre
+//
+//  Created by Luiz Camargo on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PruebaMercadiLibreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
